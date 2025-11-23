14f386eddb246c928c72e137c8404c4c0079ae40
Lucas Lange <53273617+lulange@users.noreply.github.com>
2025-11-23 15:53:31 +0000

    add minimal operator implementation

---
+++ presidio-anonymizer/presidio_anonymizer/operators/initial.py
@@ -0,0 +1,25 @@
+"""Replaces the PII text entity with initials of the text."""
+
+from typing import Dict
+
+from presidio_anonymizer.operators import Operator, OperatorType
+
+
+class Initial(Operator):
+    """Replace the string with initials."""
+
+    def operate(self, text: str = None, params: Dict = None) -> str:
+        """Take the text, and return the first letter of each word found with periods between each."""
+        return ""
+
+    def validate(self, params: Dict = None) -> None:
+        """Initial does not require any parameters so no validation is needed."""
+        pass
+
+    def operator_name(self) -> str:
+        """Return operator name."""
+        return "initial"
+
+    def operator_type(self) -> OperatorType:
+        """Return operator type."""
+        return OperatorType.Anonymize
